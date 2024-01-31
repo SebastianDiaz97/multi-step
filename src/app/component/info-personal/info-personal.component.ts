7be039e1dc090056af930af3af866a791803aee3
SebastianDiaz97 <sdiazr1997@gmail.com>
2024-01-31 03:47:32 -0300

    create info-personal

--- src/app/component/info-personal/info-personal.component.ts
+++ src/app/component/info-personal/info-personal.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./info-personal.component.css']
 })
 export class InfoPersonalComponent {
-
+  submit(event: Event){
+    event.preventDefault()
+  }
 }
